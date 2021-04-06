3dc6b427c277057d37331777327a7cefa4ab7b7e
Brian Stark <BStarkit@gmail.com>
2021-04-06 07:47:34 +0100

    Final

--- Backup.ps1
+++ Backup.ps1
@@ -1,8 +1,16 @@
 Write-Host "Copying Document files"
-robocopy "$env:UserProfile\Documents" \\control\paperwork\Docs /E /MIR /MT:20
+robocopy "$env:UserProfile\Documents" \\control\paperwork\Docs /E /Z
 Write-Host "Copying Picture files"
-robocopy "$env:UserProfile\Pictures" \\control\paperwork\Pics /E /MIR /MT:20
+robocopy "$env:UserProfile\Pictures" \\control\paperwork\Pics /E /Z
 Write-Host "Copying Download files"
-robocopy "$env:UserProfile\Downloads" \\control\paperwork\Downloads /E /MIR /MT:20
+robocopy "$env:UserProfile\Downloads" \\control\paperwork\Downloads /E /Z
 Write-Host "Copying Script files"
-robocopy "$env:UserProfile\Git" \\control\paperwork\Git /E /MIR /MT:20
+robocopy "$env:UserProfile\Git" \\control\paperwork\Git /E /Z
+Write-Host "Updating Document files"
+robocopy \\control\paperwork\Docs "$env:UserProfile\Documents" /E /Z
+Write-Host "Updating Picture files"
+robocopy \\control\paperwork\Pics "$env:UserProfile\Pictures" /E /Z
+Write-Host "Updating Download files"
+robocopy \\control\paperwork\Downloads "$env:UserProfile\Downloads" /E /Z
+Write-Host "Updating Script files"
+robocopy \\control\paperwork\Git "$env:UserProfile\Git" /E /Z
