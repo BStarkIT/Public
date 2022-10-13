f1eb8b608bb795c8ae93c910a5baa350e113b599
Brian Stark <BStarkit@gmail.com>
2022-10-13 22:04:23 +0100

    Update Thursday 10/13/2022

--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,37 @@
-$env:PSModulePath = $Env:PSModulePath+";D:\git\Modules\"
+CLS
+Remove-Variable -Force HOME
+Set-Variable HOME "D:\Git"
+$env:PSModulePath = $Env:PSModulePath+";D:\git\PSModules\"
 New-Alias -Name tray -value D:\git\Public\Tray.ps1
-New-Alias -Name F15 -value D:\git\Public\F15.ps1
 Set-Alias -Name np -Value "C:\Program Files\Notepad++\notepad++.exe"
 $Major = ((Get-Variable PSVersionTable -ValueOnly).PSVersion).Major
 $Minor = ((Get-Variable PSVersionTable -ValueOnly).PSVersion).Minor
 $Patch = ((Get-Variable PSVersionTable -ValueOnly).PSVersion).Patch
 $PS = ($Major,$Minor,$Patch) -Join "."
-$Env:PATH = "D:\git\Storage;$Env:PATH"
+$Env:PATH = "D:\git;$Env:PATH"
+$NTIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$NTPrincipal = new-object Security.Principal.WindowsPrincipal $NTIdentity
+$IsAdmin = $NTPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$hostversion="v$($Host.Version.Major).$($Host.Version.Minor)"
+$hostname = $host.Name
+IF ($IsAdmin)
+{
+    if($hostname -eq "ConsoleHost")
+    {
+        $host.ui.RawUI.WindowTitle = "StarkIT.Link PowerShell $hostversion - Running as ROOT"
+    } else {
+        $host.ui.RawUI.WindowTitle = "StarkIT.Link PowerShell ISE $hostversion - Running as ROOT"
+    }
+}
+Else
+{
+    if($hostname -eq "ConsoleHost")
+    {
+        $host.ui.RawUI.WindowTitle = "StarkIT.Link PowerShell $hostversion"
+    
+    } else { 
+        $host.ui.RawUI.WindowTitle = "StarkIT.Link PowerShell ISE $hostversion"
+    }
+}
 Write-Host "PowerShell V$PS - StarkIT.link Build"
 IP
