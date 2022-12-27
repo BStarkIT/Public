bd2b9d2503fc15a59f016d6d6d521160013bbcb8
Brian Stark <BStarkit@gmail.com>
2022-12-27 15:35:08 +0000

    update

--- profile.ps1
+++ profile.ps1
@@ -1,12 +1,27 @@
+  <#
+ .SYNOPSIS
+  Custom profile for Brian Stark of BStarkIT
+
+ .NOTES
+  Script written by Brian Stark of BStarkIT 
+
+  .DESCRIPTION
+  written by BStark
+
+ .LINK
+  Scripts can be found at:
+  https://github.com/BStarkIT
+
+#>
 Clear-Host
-$env:PSModulePath = $Env:PSModulePath+";D:\git\PSModules\"
-New-Alias -Name tray -value D:\git\Public\Tray.ps1
+$env:PSModulePath = $Env:PSModulePath+";C:\PS\PSModules\"
+New-Alias -Name tray -value C:\PS\Public\Tray.ps1
 Set-Alias -Name np -Value "C:\Program Files\Notepad++\notepad++.exe"
 $Major = ((Get-Variable PSVersionTable -ValueOnly).PSVersion).Major
 $Minor = ((Get-Variable PSVersionTable -ValueOnly).PSVersion).Minor
 $Patch = ((Get-Variable PSVersionTable -ValueOnly).PSVersion).Patch
 $PS = ($Major,$Minor,$Patch) -Join "."
-$Env:PATH = "D:\git;$Env:PATH"
+$Env:PATH = "C:\PS;$Env:PATH"
 $NTIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $NTPrincipal = new-object Security.Principal.WindowsPrincipal $NTIdentity
 $IsAdmin = $NTPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
